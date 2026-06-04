eb533168698f935533b30287ae8d968d49799742
pratiksgit123 <pratikjadhav0108@gmail.com>
2026-06-04 22:28:56 +0530

    Admin login page done test

---
+++ frontend/src/pages/admin/AdminLogin.jsx
@@ -0,0 +1,58 @@
+import "../../styles/admin.css";
+import { useNavigate } from "react-router-dom";
+
+function AdminLogin() {
+  const navigate = useNavigate();
+  return (
+    <div className="login-container">
+      <div className="login-card">
+
+        <div className="login-icon">
+          🔐
+        </div>
+
+        <h2 className="login-title">
+          Travel Booking System
+        </h2>
+
+        <p className="login-subtitle">
+          Admin Portal Login
+        </p>
+
+        <div className="mb-3">
+          <label className="form-label">
+            Email Address
+          </label>
+
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter Email"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="form-label">
+            Password
+          </label>
+
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter Password"
+          />
+        </div>
+
+        <button
+  className="btn login-btn w-100"
+  onClick={() => navigate("/dashboard")}
+  >
+     Login
+  </button>
+    
+      </div>
+    </div>
+  );
+}
+
+export default AdminLogin;
