ab1b87e6b30b54ca0faa1200e8d23e226f5c4a7e
ashishshinde27 <ashishshindeas27@gmail.com>
2026-06-04 22:18:28 +0530

    First change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
 
-import './App.css'
+// import Login from './pages/Login';
+import ChooseRegister from './pages/ChooseRegister';
+import AgentRegister from "./pages/AgentRegister";
+import AgentDashboard from "./pages/AgentDashboard";
+import AddBus from "./pages/AddBus";
+import ManageTrips from "./pages/ManageTrips";
+import ViewBookings from "./pages/ViewBookings";
+import PassengerList from "./pages/PassengerList";
+import BusStatus from "./pages/BusStatus";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-
-    </>
-  )
+    <Routes>
+      {/* <Route path="/" element={<Login />} /> */}
+      <Route path="/register" element={<ChooseRegister />} />
+      <Route path="/register/agent" element={<AgentRegister />} />
+      <Route path="/agent/dashboard" element={<AgentDashboard />} />
+      <Route path="/agent/add-bus" element={<AddBus />} />
+      <Route path="/agent/manage-routes" element={<ManageTrips />} />
+      <Route path="/agent/view-bookings" element={<ViewBookings />} />
+      <Route path="/agent/passengers/:tripId" element={<PassengerList />} />
+      <Route path="/agent/bus-status" element={<BusStatus />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
